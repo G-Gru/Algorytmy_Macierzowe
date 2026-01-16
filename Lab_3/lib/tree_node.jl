bf4885120370aab948b40a91bc61f96084ed2088
krotka <oliwiapolonia@gmail.com>
2026-01-16 16:59:02 +0100

    matrix-matrix addition (untested)

--- Lab_3/lib/tree_node.jl
+++ Lab_3/lib/tree_node.jl
@@ -5,7 +5,7 @@ export TreeNode
 mutable struct TreeNode{T}
     rank::Int
     size::NTuple{4,Int}                 # (r_min, r_max, c_min, c_max)
-    singularvalues::Vector{T} | nothing
+    singularvalues::Union{Vector{T}, Nothing}
     U::Matrix{T}
     V::Matrix{T}                        # stores (D*Vᵀ) truncated
     sons::Vector{TreeNode{T}}
--- Lab_3/src/operations.jl
+++ Lab_3/src/operations.jl
@@ -3,7 +3,7 @@ module Operations
 import ..create_tree
 import ..tree_node
 
-export matrix_vector_mult
+export matrix_vector_mult, matrix_matrix_add
 
 function matrix_vector_mult(v, X)
     rows = size(X, 1)
@@ -28,10 +28,13 @@ function matrix_vector_mult(v, X)
 end
 
 function new_mat(u, v, sons)
-    tree_node.TreeNode{T}(
+    tree_node.TreeNode{eltype(u)}(
         size(u, 2),
-        (1, )
-        # suggested TODO - simple constructor for compressed matrices
+        (1, size(u, 1), 1, size(v, 2)),
+        nothing,
+        u,
+        v,
+        sons
     )
 end
 
@@ -43,20 +46,19 @@ function matrix_matrix_add(v, w, r=1, m=5.0)  # TODO are these default values al
     # both matrices are zeroed leaves
     if isempty(v.sons) && isempty(w.sons) && v.rank == 0 && w.rank == 0
         # Return a zeroed matrix of proper dimensions - TODO is this correct?
-        return tree_node.TreeNode{T}(0, (r_min, r_max, c_min, c_max), T[],
-                                    zero(v.U), zero(v.V), tree_node.TreeNode{T}[])
+        return new_mat(zero(v.U), zero(v.V), [])
     end
 
     # both matrices are nonzero leaves
-    if isempty(v.sons) && isempty(w.sons) && v.rank =! 0 && w.rank =! 0
+    if isempty(v.sons) && isempty(w.sons) && v.rank != 0 && w.rank != 0
         U = [v.U w.U]
         V = [v.V; w.V]
 
         # reconstructs a matrix from the values, then compresses it (costly)
-        m = U * V
-        rows, cols = size(m, 1), size(m, 2)
+        ans = U * V
+        rows, cols = size(ans, 1), size(ans, 2)
 
-        return create_tree.compression_tree(m, 1, rows, 1, cols, r, m)
+        return create_tree.compression_tree(ans, 1, rows, 1, cols, r, m)
     end
 
     sons_of = function(m)
@@ -64,22 +66,22 @@ function matrix_matrix_add(v, w, r=1, m=5.0)  # TODO are these default values al
             umid = size(m.U, 1) ÷ 2
             vmid = size(m.V, 2) ÷ 2
 
-            u1, u2 = u[1:umid,:], u[umid+1:end,:]
-            v1, v2 = v[:,1:vmid], v[:,vmid+1:end]
+            u1, u2 = m.U[1:umid,:], m.U[umid+1:end,:]
+            v1, v2 = m.V[:,1:vmid], m.V[:,vmid+1:end]
 
-            #TODO
+            lu = new_mat(u1, v1, [])
+            ru = new_mat(u1, v2, [])
+            ll = new_mat(u2, v1, [])
+            rl = new_mat(u2, v2, [])
+
+            [lu, ru, ll, rl]
         else
             m.sons
         end
     end
 
-    # none of the matrices are leaves, recurse them
-    if !isempty(v.sons) and !isempty(w.sons)
-        sons = [matrix_matrix_add(a, b) for (a, b) in zip(v.sons, w.sons)]
-        return tree_node.TreeNode{T}() # TODO construct
-    end
-
-    # only one of the matrices is a leaf, split it
+    sons = [matrix_matrix_add(a, b) for (a, b) in zip(sons_of(v), sons_of(w))]
+    return new_mat(zeros(T, 0, 0), zeros(T, 0, 0), sons)
 end
 
 end
